97c51c42fc8359c88760c1a459328da149304289
Seyoung Park <seyoungpark@hanyang.ac.kr>
2025-07-07 19:29:50 +0900

    tap2 상단하단 패딩 추가

--- app/src/main/java/com/example/madcamp1/data/ProblemListItem.kt
+++ app/src/main/java/com/example/madcamp1/data/ProblemListItem.kt
@@ -1,6 +1,6 @@
 package com.example.madcamp1.data
 
 sealed class ProblemListItem {
-    data class Header(val title: String) : ProblemListItem()
+    data class Header(val title: String, var isExpanded: Boolean = true) : ProblemListItem()
     data class Item(val name: String, val id: Int) : ProblemListItem()
 }
