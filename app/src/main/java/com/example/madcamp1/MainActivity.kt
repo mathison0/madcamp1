7b6524b5ef67e15240aaa1bc05a42904e37f906f
Seyoung Park <seyoungpark@hanyang.ac.kr>
2025-07-08 17:11:45 +0900

    다크모드막음

--- app/src/main/java/com/example/madcamp1/MainActivity.kt
+++ app/src/main/java/com/example/madcamp1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.madcamp1
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
